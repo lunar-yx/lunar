b49bcf22bf006e6a75a0bddf202496a7f0ee3e77
Tedu <1157501981@qq.com>
2019-10-23 10:35:48 +0800

    init commit

---
+++ exercise02.py
@@ -0,0 +1,9 @@
+quarter = input("请输入季度：")
+if quarter == "春":
+    print("1月2月3月")
+elif quarter == "夏":
+    print("4月5月6月")
+elif quarter == "秋":
+    print("7月8月9月")
+elif quarter == "冬":
+    print("10月11月12月")
